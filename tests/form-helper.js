21b361e5a471247d16718cfa94b412c60b72fc94
Rafał Mikołajun <rafal@mikoweb.pl>
2016-06-21 21:47:34 +0200

    testowanie metody FormHelper::removeExtremeBrackets()

--- tests/form-helper.js
+++ tests/form-helper.js
@@ -50,5 +50,24 @@
                 expect(helper.removePrefix('address[city]')).to.be('address[city]');
             });
         });
+        describe('#removeExtremeBrackets()', function () {
+            var helper = new Backbone.form.FormHelper(document.createElement('FORM'));
+
+            it('Powinno wywalić nawiasy na początku i końcu', function () {
+                expect(helper.removeExtremeBrackets('[last_name]')).to.be('last_name');
+            });
+
+            it('Wyrażenie bez nawiasów jest nienaruszone', function () {
+                expect(helper.removeExtremeBrackets('lorem ipsum')).to.be('lorem ipsum');
+            });
+
+            it('Wyrażanie z więcej niż jednym domknięciem jest nienaruszone', function () {
+                expect(helper.removeExtremeBrackets('[foo][bar]')).to.be('[foo][bar]');
+            });
+
+            it('Wyrażenie z jednym domknięciem ale z więcej niż jednym otwarciem zostanie przetworzone', function () {
+                expect(helper.removeExtremeBrackets('[foo[bar]')).to.be('foo[bar');
+            });
+        });
     });
 }());
