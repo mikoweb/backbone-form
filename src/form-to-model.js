5353636e7c89328f1b5f7493549ab839d527aeec
Rafał Mikołajun <rafal@mikoweb.pl>
2016-06-26 18:11:37 +0200

    konstruktor FormToModel - jest odpowiedzialny nanoszenie danych z formularza do modelu

---
+++ src/form-to-model.js
@@ -0,0 +1,18 @@
+/**
+ * @author Rafał Mikołajun <rafal@mikoweb.pl>
+ * @license GPLv3
+ */
+(function () {
+    "use strict";
+
+    Backbone.form = Backbone.form || {};
+
+    /**
+     * @param {Object} options
+     * @constructor
+     */
+    function FormToModel (options) {
+    }
+
+    Backbone.form.FormToModel = FormToModel;
+}());
