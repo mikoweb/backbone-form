d2d59a382c56e86de3c4278163d2c9df6dddf999
Rafał Mikołajun <rafal@mikoweb.pl>
2016-06-30 21:00:35 +0200

    klasa FormToModel - jeszcze nie testowana

--- src/form-to-model.js
+++ src/form-to-model.js
@@ -19,11 +19,82 @@
         this.model = data.model;
         this.form = data.form;
         this.options = _.defaults(options, {
-            prefix: null
+            naming: 'brackets',
+            separator: null,
+            auto: true,
+            keepPrefix: false
         });
 
-        this.formHelper = new Backbone.form.FormHelper(this.form, this.options.prefix);
+        this.formHelper = new Backbone.form.FormHelper(this.form, this.options.naming, this.options.separator);
     }
+
+    /**
+     * @param {Object} target
+     * @param {Object} source
+     *
+     * @returns {Object}
+     */
+    function mergeObject (target, source) {
+        var prop;
+
+        for (prop in source) {
+            if (source.hasOwnProperty(prop)) {
+                if (target[prop] && typeof source[prop] === 'object') {
+                    mergeObject(target[prop], source[prop]);
+                } else {
+                    target[prop] = source[prop];
+                }
+            }
+        }
+
+        return target;
+    }
+
+    /**
+     * @param {String} message
+     * @constructor
+     */
+    FormToModel.prototype.WildcardValueError = function (message) {
+        this.name = 'Backbone.form.FormToModel.WildcardValueError';
+        this.message = message;
+    };
+
+    FormToModel.prototype.WildcardValueError.prototype = new Error();
+
+    FormToModel.prototype.bind = function () {
+        var inputs = this.form.querySelectorAll('[name]'), i;
+
+        this.model.clear();
+
+        for (i = 0; i < inputs.length; i++) {
+            this.bindControl(inputs[i].getAttribute('name'));
+        }
+    };
+
+    /**
+     * @param {String} name
+     */
+    FormToModel.prototype.bindControl = function (name) {
+        var value = this.formHelper.getObjectFromName(name, this.options.keepPrefix),
+            keys = _.keys(value), key, oldValue;
+
+        if (keys.length > 1) {
+            throw new this.WildcardValueError('Control "' + name + '" has ' + keys.length + ' values');
+        }
+
+        if (keys.length) {
+            key = keys[0];
+            oldValue = this.model.get(key);
+
+            if (value[key] !== null) {
+                if (typeof oldValue === 'object' && typeof value[key] === 'object') {
+                    this.model.set(key, mergeObject(oldValue, value[key]));
+                } else {
+                    this.model.set(key, value[key]);
+                }
+            }
+        }
+    };
 
     Backbone.form.FormToModel = FormToModel;
 }());
