ce7ab5f637f495fc65d01000918b8cc709931ca7
Rafał Mikołajun <rafal@mikoweb.pl>
2016-07-08 16:37:05 +0200

    konstruktor model to form

---
+++ src/model-to-form.js
@@ -0,0 +1,20 @@
+/**
+ * @author Rafał Mikołajun <rafal@mikoweb.pl>
+ * @license GPLv3
+ */
+(function () {
+    "use strict";
+
+    Backbone.form = Backbone.form || {};
+
+    /**
+     * @param {Backbone.Model} model
+     * @param {HTMLElement|jQuery} form
+     * @constructor
+     */
+    function ModelToForm (model, form) {
+        // @todo
+    }
+
+    Backbone.form.ModelToForm = ModelToForm;
+}());
