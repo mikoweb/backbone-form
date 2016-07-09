1cab1f23367deeda9d22c506a0b72ac46814b39e
Rafał Mikołajun <rafal@mikoweb.pl>
2016-07-09 21:45:48 +0200

    interfejs klasy ModelToForm

--- src/model-to-form.js
+++ src/model-to-form.js
@@ -10,11 +10,60 @@
     /**
      * @param {Backbone.Model} model
      * @param {HTMLElement|jQuery} form
+     * @param {Object} [options]
      * @constructor
      */
-    function ModelToForm (model, form) {
-        // @todo
+    function ModelToForm (model, form, options) {
+        var data = Backbone.form.validModelForm(model, form);
+
+        this._auto = false;
+        this.model = data.model;
+        this.form = data.form;
+        this.options = _.defaults(options || {}, Backbone.form.getModelToFormDefaults());
+        this.formHelper = new Backbone.form.FormHelper(this.form, this.options.naming, this.options.separator);
+        this.auto(this.options.auto);
     }
+
+    ModelToForm.prototype.bind = function () {
+    };
+
+    /**
+     * @param {String} attr
+     */
+    ModelToForm.prototype.bindAttribute = function (attr) {
+    };
+
+    /**
+     * @return {Backbone.Model}
+     */
+    ModelToForm.prototype.getModel = function () {
+        return this.model;
+    };
+
+    /**
+     * @return {HTMLElement}
+     */
+    ModelToForm.prototype.getForm = function () {
+        return this.form;
+    };
+
+    /**
+     * @param {Boolean} auto
+     */
+    ModelToForm.prototype.auto = function (auto) {
+        if (typeof auto !== 'boolean') {
+            throw new TypeError('Auto must be boolean');
+        }
+
+        this._auto = auto;
+    };
+
+    /**
+     * @returns {Boolean}
+     */
+    ModelToForm.prototype.isAuto = function () {
+        return this._auto;
+    };
 
     Backbone.form.ModelToForm = ModelToForm;
 }());
